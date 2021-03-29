3628e9d11d5e566dff9d7453de2609972cf8e9ab
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-03-29 16:54:52 +0100

    Media Queries
    + some css fixes

--- frontend/src/components/pageNotFound/PageNotFound.js
+++ frontend/src/components/pageNotFound/PageNotFound.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './PageNotFound.css';
-
+import React from "react";
+import "./PageNotFound.css";
 
 function PageNotFound() {
-    return (
-        <div>
-            <h1 className="pageNotFound"> Oops!</h1>
-            <h2 className="notExist">This page does not exist!</h2>
-            <a href="/" className="NotFound-Homepage">
-            Take me home!
-            </a>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="pageNotFound"> Oops!</h1>
+      <h2 className="notExist">This page does not exist!</h2>
+      <div className="take-me-home">
+        <a href="/" className="takemehome">
+          Take me home!
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
