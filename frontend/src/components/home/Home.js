68274d8fc2a9809804412b4c68223ef267e7f020
Evan Morris <evanmorris97@gmail.com>
2021-03-29 18:07:47 +0100

    created user history pages
    created the component and css for the user history page, created a table and some css styling.
    also added some styling to fix the position of the footer.

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -8,11 +8,29 @@ function Home() {
         <BrowserRouter>
             <div className="home-container">
 
-                <Section heading="Play a Round"
+                <Section
+                    heading="Check the Leaderboard!"
+                    text="How competitive are you? See how you are doing against others!"
+                    link="/leaderboard"
+                    button="Leaderboard"
+                />
+
+                <Section 
+                    heading="Try Another Quiz!"
                     text="Test your knowledge - start a new quiz"
                     link="/quiz"
-                    button="Start New Quiz" />
+                    button="Start New Quiz" 
+                />
                 
+                <Section 
+                    heading="Check Your History!"
+                    text="Check Your Results: How are you doing?"
+                    link="/history"
+                    button="Results"
+                />
+
+
+
                 {/* we do not need this random quiz card until we add the functionality to start a random quiz for a user */}
                 {/* <Section heading="Random Quiz"
                     text="I don't care just give me my quiz fix!"
