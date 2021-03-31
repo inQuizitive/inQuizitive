f6d70216307fd669a8e132e45c1c8e68eb836289
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-03-31 12:11:20 +0100

    Footer media query

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,44 +1,64 @@
 import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div className="footer-wrapper">
-            <footer>
-                <div className="footer-content">
-                    <div className="about-us-navigation">
-                        <nav className="about-us-nav">
-                            <div className="about-us-title">
-                                <h5 className="about-us-footer-title">About us</h5>
-                            </div>
-                            <div className="about-us-menu">
-                                <div className="menu">
-                                    <a href="https://github.com/evanmorrisdev" target="_blank">Evan</a><br></br>
-                                    <a href="https://github.com/LRWright" target="_blank">Le-Ann</a><br></br>
-                                    <a href="https://github.com/lisarawlings" target="_blank">Lisa</a><br></br>
-                                    <a href="https://github.com/ruarplum" target="_blank">Ruaraidh</a><br></br>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
-                    <div className="footer-title">
-                        <h3>in<span>Quiz</span>itive</h3>
-                    </div>
-                    <div className="resources-navigation">
-                        <nav className="resources-nav">
-                            <div className="resource-title">
-                                <h5>Resources</h5>
-                            </div>
-                            <div className="resources-menu">
-                                <div className="menu">
-                                    <a href="https://github.com/inQuizitive/inQuizitive" target="_blank">Code Source</a><br></br>
-                                    <a href="/frequently-asked-questions">FAQs</a>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
+  return (
+    <div className="footer-wrapper">
+      <footer>
+        <div className="footer-content">
+          <div className="about-us-navigation">
+            <nav className="about-us-nav">
+              <div className="about-us-title">
+                <h5 className="about-us-footer-title">About us</h5>
+              </div>
+              <div className="about-us-menu">
+                <div className="menu">
+                  <a href="https://github.com/evanmorrisdev" class="Evan">
+                    Evan
+                  </a>
+                  <br></br>
+                  <a href="https://github.com/LRWright" class="Le-Ann">
+                    Le-Ann
+                  </a>
+                  <br></br>
+                  <a href="https://github.com/lisarawlings" class="Lisa">
+                    Lisa
+                  </a>
+                  <br></br>
+                  <a href="https://github.com/ruarplum" class="Ruaraidh">
+                    Ruaraidh
+                  </a>
+                  <br></br>
                 </div>
-            </footer>
+              </div>
+            </nav>
+          </div>
+          <div className="footer-title">
+            <h3>
+              in<span>Quiz</span>itive
+            </h3>
+          </div>
+          <div className="resources-navigation">
+            <nav className="resources-nav">
+              <div className="resource-title">
+                <h5>Resources</h5>
+              </div>
+              <div className="resources-menu">
+                <div className="menu">
+                  <a
+                    href="https://github.com/inQuizitive/inQuizitive"
+                    target="_blank"
+                  >
+                    Code Source
+                  </a>
+                  <br></br>
+                  <a href="/frequently-asked-questions">FAQs</a>
+                </div>
+              </div>
+            </nav>
+          </div>
         </div>
-    )
-}
+      </footer>
+    </div>
+  );
+};
 export default Footer;
